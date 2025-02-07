e891274204cf9b12a03934ee87d63165b86d1932
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2025-02-07 16:57:53 +0300

    Update Mapping between processes

--- mapping_between_processes.cpp
+++ mapping_between_processes.cpp
@@ -15,134 +15,109 @@ const wchar_t* dataWrittenEventName = L"Local\DataWrittenEvent";
 const wchar_t* dataReadEventName = L"Local\DataReadEvent";
 const int BUF_SIZE = 512;
 
-void WriteToSharedMemory()
-{
+class SharedMemoryManager {
+public:
+    SharedMemoryManager(bool isWriter) : isWriter(isWriter) {
 #ifdef _WIN32
-    HANDLE hMapFile = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, BUF_SIZE, shmName);
-    if (!hMapFile)
-    {
-        std::cerr << "CreateFileMapping error: " << GetLastError() << std::endl;
-        return;
-    }
-    char* pBuf = static_cast<char*>(MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, BUF_SIZE));
+        if (isWriter) {
+            hMapFile = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, BUF_SIZE, shmName);
+        }
+        else {
+            hMapFile = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, shmName);
+        }
+        if (!hMapFile) {
+            std::cerr << "File mapping error: " << GetLastError() << std::endl;
+            return;
+        }
+        pBuf = static_cast<char*>(MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, BUF_SIZE));
+        hDataWritten = CreateEventW(NULL, FALSE, FALSE, dataWrittenEventName);
+        hDataRead = CreateEventW(NULL, FALSE, FALSE, dataReadEventName);
 #else
-    int shmFd = shm_open(shmName, O_CREAT | O_RDWR, 0666);
-    ftruncate(shmFd, BUF_SIZE);
-    char* pBuf = static_cast<char*>(mmap(0, BUF_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shmFd, 0));
+        if (isWriter) {
+            shmFd = shm_open(shmName, O_CREAT | O_RDWR, 0666);
+            ftruncate(shmFd, BUF_SIZE);
+        }
+        else {
+            shmFd = shm_open(shmName, O_RDWR, 0666);
+        }
+        pBuf = static_cast<char*>(mmap(0, BUF_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shmFd, 0));
+        hDataWritten = sem_open(dataWrittenEventName, O_CREAT, 0666, 0);
+        hDataRead = sem_open(dataReadEventName, O_CREAT, 0666, 0);
 #endif
-
-    if (!pBuf)
-    {
-        std::cerr << "Memory mapping error" << std::endl;
-        return;
-    }
-
-#ifdef _WIN32
-    HANDLE hDataWritten = CreateEventW(NULL, FALSE, FALSE, dataWrittenEventName);
-    HANDLE hDataRead = CreateEventW(NULL, FALSE, FALSE, dataReadEventName);
-#else
-    sem_t* hDataWritten = sem_open(dataWrittenEventName, O_CREAT, 0666, 0);
-    sem_t* hDataRead = sem_open(dataReadEventName, O_CREAT, 0666, 0);
-#endif
-
-    const char* message = "Hello from shared memory!";
-#ifdef _WIN32
-    strcpy_s(pBuf, BUF_SIZE, message);
-#else
-    strcpy(pBuf, message);
-#endif
-    std::cout << "Data written to shared memory. Waiting for reading..." << std::endl;
-
-#ifdef _WIN32
-    SetEvent(hDataWritten);
-    WaitForSingleObject(hDataRead, INFINITE);
-#else
-    sem_post(hDataWritten);
-    sem_wait(hDataRead);
-#endif
-
-#ifdef _WIN32
-    UnmapViewOfFile(pBuf);
-    CloseHandle(hDataWritten);
-    CloseHandle(hDataRead);
-    CloseHandle(hMapFile);
-#else
-    munmap(pBuf, BUF_SIZE);
-    close(shmFd);
-    shm_unlink(shmName);
-    sem_close(hDataWritten);
-    sem_close(hDataRead);
-    sem_unlink(dataWrittenEventName);
-    sem_unlink(dataReadEventName);
-#endif
-}
-
-void ReadFromSharedMemory()
-{
-#ifdef _WIN32
-    HANDLE hMapFile = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, shmName);
-    char* pBuf = static_cast<char*>(MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, BUF_SIZE));
-#else
-    int shmFd = shm_open(shmName, O_RDONLY, 0666);
-    char* pBuf = static_cast<char*>(mmap(0, BUF_SIZE, PROT_READ, MAP_SHARED, shmFd, 0));
-#endif
-
-    if (!pBuf)
-    {
-        std::cerr << "Memory mapping error" << std::endl;
-        return;
     }
 
+    ~SharedMemoryManager() {
 #ifdef _WIN32
-    HANDLE hDataWritten = OpenEventW(EVENT_ALL_ACCESS, FALSE, dataWrittenEventName);
-    HANDLE hDataRead = OpenEventW(EVENT_ALL_ACCESS, FALSE, dataReadEventName);
+        UnmapViewOfFile(pBuf);
+        CloseHandle(hMapFile);
+        CloseHandle(hDataWritten);
+        CloseHandle(hDataRead);
 #else
-    sem_t* hDataWritten = sem_open(dataWrittenEventName, 0);
-    sem_t* hDataRead = sem_open(dataReadEventName, 0);
+        munmap(pBuf, BUF_SIZE);
+        close(shmFd);
+        sem_close(hDataWritten);
+        sem_close(hDataRead);
+        if (isWriter) {
+            shm_unlink(shmName);
+            sem_unlink(dataWrittenEventName);
+            sem_unlink(dataReadEventName);
+        }
 #endif
-
-    std::cout << "Waiting for data..." << std::endl;
-#ifdef _WIN32
-    WaitForSingleObject(hDataWritten, INFINITE);
-#else
-    sem_wait(hDataWritten);
-#endif
-
-    std::cout << "Received data: " << pBuf << std::endl;
-    memset(pBuf, 0, BUF_SIZE);
-
-#ifdef _WIN32
-    SetEvent(hDataRead);
-    UnmapViewOfFile(pBuf);
-    CloseHandle(hDataWritten);
-    CloseHandle(hDataRead);
-    CloseHandle(hMapFile);
-#else
-    sem_post(hDataRead);
-    munmap(pBuf, BUF_SIZE);
-    close(shmFd);
-    sem_close(hDataWritten);
-    sem_close(hDataRead);
-#endif
-}
-
-int main()
-{
-#ifdef _WIN32
-    HANDLE hMapFile = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, shmName);
-    bool isFirstProcess = (hMapFile == NULL);
-#else
-    int shmFd = shm_open(shmName, O_RDWR, 0666);
-    bool isFirstProcess = (shmFd == -1);
-#endif
-
-    if (isFirstProcess)
-    {
-        WriteToSharedMemory();
     }
-    else
-    {
-        ReadFromSharedMemory();
+
+    void Write(const char* message) {
+        if (!isWriter) return;
+#ifdef _WIN32
+        strcpy_s(pBuf, BUF_SIZE, message);
+        SetEvent(hDataWritten);
+        WaitForSingleObject(hDataRead, INFINITE);
+#else
+        strcpy(pBuf, message);
+        sem_post(hDataWritten);
+        sem_wait(hDataRead);
+#endif
+    }
+
+    void Read() {
+        if (isWriter) return;
+#ifdef _WIN32
+        WaitForSingleObject(hDataWritten, INFINITE);
+#else
+        sem_wait(hDataWritten);
+#endif
+        std::cout << "Received data: " << pBuf << std::endl;
+        memset(pBuf, 0, BUF_SIZE);
+#ifdef _WIN32
+        SetEvent(hDataRead);
+#else
+        sem_post(hDataRead);
+#endif
+    }
+
+private:
+    bool isWriter;
+#ifdef _WIN32
+    HANDLE hMapFile, hDataWritten, hDataRead;
+#else
+    int shmFd;
+    sem_t* hDataWritten, * hDataRead;
+#endif
+    char* pBuf;
+};
+
+int main() {
+#ifdef _WIN32
+    bool isFirstProcess = (OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, shmName) == NULL);
+#else
+    bool isFirstProcess = (shm_open(shmName, O_RDWR, 0666) == -1);
+#endif
+
+    SharedMemoryManager shmManager(isFirstProcess);
+    if (isFirstProcess) {
+        shmManager.Write("Hello from shared memory!");
+    }
+    else {
+        shmManager.Read();
     }
 
     std::cout << (isFirstProcess ? "First process: exit" : "Second process: exit") << std::endl;
