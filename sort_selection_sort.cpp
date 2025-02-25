9d3133dc9f9473eead6ccd2d5c06adb34f845104
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2025-02-25 11:43:26 +0300

    Add Selection Sort

---
+++ sort_selection_sort.cpp
@@ -0,0 +1,16 @@
+void selectionSort(std::vector<int>& arr)
+{
+    int n = arr.size();
+    for (int i = 0; i < n - 1; ++i)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        std::swap(arr[i], arr[minIndex]);
+    }
+}
